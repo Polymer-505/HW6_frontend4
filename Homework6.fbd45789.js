1fd5984867feacafc558e07ff6bec0b576887845
Polymer-505 <korololeksandr306@gmail.com>
2026-06-10 17:30:15 +0300

    Updates

---
+++ Homework6.fbd45789.js
@@ -0,0 +1,2 @@
+let o=document.getElementById("theme-btn"),e=!1;o.addEventListener("click",()=>{(e=!e)?(document.body.style.backgroundColor="#222222",document.body.style.color="#f5f5f5"):(document.body.style.backgroundColor="#ffffff",document.body.style.color="#333333")});
+//# sourceMappingURL=Homework6.fbd45789.js.map
